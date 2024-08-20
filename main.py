0e1881ff9c2ddb3ed2bb1d3bebf7be378aed1cac
SashaSU123 <ggmarkspenser1991@gmail.com>
2024-08-20 17:20:51 +0300

    final py with words and characters count

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ path = "books/frankenstein.txt"
 def main():
     with open(path) as f:
         file_contents = f.read()
-    print(1)
+    print(f"--- Begin report of {path} ---")
 main()
  
 def count_words():
@@ -10,7 +10,7 @@ def count_words():
         file_contents = f.read()
         words = file_contents.split()
         count_words = len(words)
-    print(count_words)
+    print(f"{count_words} words found in the document")
 count_words()
 
 def count_character():
@@ -20,9 +20,27 @@ def count_character():
         lower_words = words.split()
         characters = {}
         for word in words:
-            if word in characters:
-                characters[word] += 1
-            else:
-                characters[word] = 1
-        print(characters)   
+            if word.isalpha() == True:
+                if word in characters:
+                    characters[word] += 1
+                else:
+                    characters[word] = 1
+        return characters
 count_character()
+
+
+dict = count_character()
+dict_list = [{"char": k, "num": v} for k, v in dict.items()]
+
+def sort_on(dict):
+    return dict["num"]
+
+dict_list.sort(reverse=True, key=sort_on)
+
+
+for i in dict_list:
+    character = i["char"]
+    number = i["num"]
+    print(f"The {character} character was found {number} times")
+
+print("--- End report ---")
