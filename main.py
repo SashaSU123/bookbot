9996758e5ad95b27e8ba5b5ac449fa69cfd9a140
SashaSU123 <ggmarkspenser1991@gmail.com>
2024-08-20 15:59:22 +0300

    added all python codes

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+path = "books/frankenstein.txt"
 def main():
-    with open("books/frankenstein.txt") as f:
+    with open(path) as f:
         file_contents = f.read()
-    print(file_contents)
+    print(1)
 main()
+ 
+def count_words():
+    with open(path) as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        count_words = len(words)
+    print(count_words)
+count_words()
+
+def count_character():
+    with open(path) as f:
+        file_contents = f.read()
+        words = file_contents.lower()
+        lower_words = words.split()
+        characters = {}
+        for word in words:
+            if word in characters:
+                characters[word] += 1
+            else:
+                characters[word] = 1
+        print(characters)   
+count_character()
